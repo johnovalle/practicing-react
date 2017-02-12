2c0ce722b780007a4451d0ca25160319791401c5
John Ovalle <johnovalle@gmail.com>
2017-02-12 14:30:07 -0500

    configure webpack to use babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
   context: __dirname + "/app",
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/scripts.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + "/app/js",
     filename: "scripts.min.js"
