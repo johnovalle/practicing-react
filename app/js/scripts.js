2bdf1d454ffac4e9a0cc3c40daa03fd12f1f38ce
John Ovalle <johnovalle@gmail.com>
2017-02-13 07:10:29 -0500

    setup hot reloading but it's not actually recompling on save

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,13 @@
-require("./script1");
+import React from 'react';
+import ReacDOM from 'react-dom';
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>React is working!</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReacDOM.render(<Layout/>, app);
