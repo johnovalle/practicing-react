1ce8ad979f0fc7643da0d9c5056d159ecc7518ad
John Ovalle <johnovalle@gmail.com>
2017-07-29 19:00:05 -0400

    super basic data management, state and passing props

--- learn_code/src/js/components/Header.js
+++ learn_code/src/js/components/Header.js
@@ -4,7 +4,7 @@ import Title from "./Header/Title";
 class Header extends React.Component {
     render() {
         return (
-            <Title />
+            <Title title={this.props.title}/>
         );
     }
 }
--- learn_code/src/js/components/Layout.js
+++ learn_code/src/js/components/Layout.js
@@ -3,14 +3,23 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 class Layout extends React.Component {
-		render() {
-				return (
-						<div>
-								<Header />
-								<Footer />
-						</div>
-				);
-		}
+	constructor() {
+		super();
+		this.state = {
+			title: "Welcome!"
+		};
+	}
+	render() {
+		setTimeout(()=>{
+			this.setState({title: "Welcome!!!!"});
+		}, 3000);
+		return (
+			<div>
+				<Header title={this.state.title}/>
+				<Footer />
+			</div>
+		);
+	}
 }
 export default Layout;
 
@@ -21,13 +30,19 @@ export default Layout;
 //     constructor() {
 //         super();
 //         this.test = 5;
+//					this.state = {name: "John"};
 //     }
 //     getVal(val) {
 //         return val * val;
 //     }
 //     render() {
+//			setTimeout(()=>{
+//				this.setState({name: "JO"});
+//			}, 1000);
+///
 //         return (
 //             <div>
+//								{this.state.name}
 //                 <Header />
 //                 <h1>It works! {this.getVal(3)} {this.test}</h1>
 //             </div>
