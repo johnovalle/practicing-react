5d6da456f1f9cf3e474f2723cbb7064fb95d7446
John Ovalle <johnovalle@gmail.com>
2017-07-29 19:12:02 -0400

    convert Title to 'dumb' component

--- learn_code/src/js/components/Header/Title.js
+++ learn_code/src/js/components/Header/Title.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default class Title extends React.Component {
-    render() {
-        return (
-            <h1>{this.props.title}</h1>
-        );
-    }
+// export default class Title extends React.Component {
+//     render() {
+//         return (
+//             <h1>{this.props.title}</h1>
+//         );
+//     }
+// }
+export default function Title(props) {
+  return (
+    <h1>{props.title}</h1>
+  );
 }
